5b2c17cc3b9ecff7c5aa36b92392f5c35373134c
unknown <evgenshtanko@ya.ru>
2020-12-08 19:46:41 +0400

    t

---
+++ js/common.js
@@ -0,0 +1,15 @@
+  $('.reviews-text').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+
+  $('.reviews-video').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
