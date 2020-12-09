bbf2f37bf5c470819a8c1aedc282a45f29a11d67
unknown <evgenshtanko@ya.ru>
2020-12-09 18:09:26 +0400

    t

--- js/common.js
+++ js/common.js
@@ -1,4 +1,6 @@
-  $('.reviews-text').slick({
+const btnRead = document.getElementById('btn-read');
+
+$('.reviews-video').slick({
     dots: true,
     infinite: true,
     speed: 500,
@@ -6,10 +8,7 @@
     cssEase: 'linear'
   });
 
-  $('.reviews-video').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
-  });
+  btnRead.onclick = function(){
+    btnRead.classList.add('d-none');
+    document.getElementById('text-read').classList.remove('d-none')
+  }
